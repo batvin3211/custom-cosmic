e51055d50f0402bc16d5d2fc68a22a2267ae00c1
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2025-07-20 19:45:45 +0000

    Create nix.sh

---
+++ files/scripts/nix.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+mkdir -p /nix && \
+	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix -o /nix/determinate-nix-installer.sh && \
+	chmod a+rx /nix/determinate-nix-installer.sh
