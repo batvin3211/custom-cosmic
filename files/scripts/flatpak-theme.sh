cc1a4a5aef93bae4595b2e09ed09db025ab63392
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2025-08-14 13:53:06 -0400

    Update flatpak-theme.sh

--- files/scripts/flatpak-theme.sh
+++ files/scripts/flatpak-theme.sh
@@ -8,4 +8,4 @@ set -oue pipefail
 # Your code goes here.
 sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro
 sudo flatpak override --filesystem=xdg-config/gtk-4.0:ro
-sudo flatpak override --env=QT_STYLE_OVERRIDE=adwaita
+sudo flatpak override --env=QT_QPA_PLATFORMTHEME=xdgdesktopportal
